4109335a65cc2deb712c2cf526ff2dd93ef20db6
unforswearing <notes@theforeverarrow.com>
2014-09-16 00:47:30 -0400

    update

--- dued.rb
+++ dued.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/ruby
 
+# dued   
+#  
+# Usage:  
+# 	dued -d <days from now>  
+# 		print and copy the date x days from now  
+# 		example - 'dued -d 2' prints the date two days from now  
+# 	dued -y <days before today>  
+# 		print and copy the date x days before today  
+# 		example - 'dued -y 5' prints the date five days before today  
+# 	dued -t  
+# 		print todays date   
+
 require 'date'
 require "time"
 
 num = ARGV[1].to_i
+tnum = ARGV[2].to_i
 day = ARGV[0]
 time = Time.now
 
@@ -27,22 +40,31 @@ system("echo #{dued} | sed 's/20..-//' | sed 's/0//g' | sed 's/-/\\//' | pbcopy 
 # the original 'dued' script has a time option. 
 # that option in this ruby script is not functioning exactly as I need it to just yet.
 # ---------------------
-# when "-h"
+# when "-w"
+# today = Date.today
 # num = num * 60 * 60
-# dued = time + num
-# system("echo #{dued} } | awk '{ print $2 }' | sed 's/:[0-9].//' | pbcopy | pbpaste")
+# duedt = time + tnum
+# duedd = today + num
+# system("echo #{duedd} + " " #{duedt} | awk '{ print $2 }' | sed 's/:[0-9].//' | pbcopy | pbpaste")
 # find a way to use AM and PM
 
-when "-help" 
-puts "dued help to be written."
 
-when nil
-puts "Dued requires an argument. Type 'dued -h' for more information"
+when "-help" # print help text
+puts "
+Usage:
+	dued -d <days from now>
+		print and copy the date x days from now
+		example - 'dued -d 2' prints the date two days from now
+	dued -y <days before today>
+		print and copy the date x days before today
+		example - 'dued -y 5' prints the date five days before today
+	dued -t
+		print todays date"
+
+when nil # when no argument is entered
+puts "Dued requires an argument. Type 'dued -help' for more information"
 
 else
-	puts "That is probably not a valid command. For a list of options dued will accept, type 'dued -h'."
+	puts "That is probably not a valid command. For a list of options dued will accept, type 'dued -help'."
 
 end
-
-# TODO: find a way to have a user enter a date and return the number of days until that date
-# TODO: fix the 'time' and 'date and time' options  
